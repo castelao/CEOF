9102b25b35388b4e6b67e800df35aacc448b4d9a
Gui <guilherme@castelao.net>
2015-01-20 12:09:54 -0800

    Moving make_animations into ceof.graphics

--- ceof/graphics.py
+++ ceof/graphics.py
@@ -181,3 +181,103 @@ def plot(eof, pc, nmode, varfrac, filename, data, limits=None, cumvarfrac=None):
     #fig.savefig(filename)
     pylab.savefig(filename)
     pylab.close()
+
+
+def make_animation(data, eofdata, t, lat, lon, outputfilename, limits = None):
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt   # For plotting graphs.
+    import pylab
+    from mpl_toolkits.basemap import Basemap
+
+    #import numpy as np
+    #import subprocess                 # For issuing commands to the OS.
+    import os
+    import sys                        # For determining the Python version.
+
+    not_found_msg = """
+    The mencoder command was not found;
+    mencoder is used by this script to make an avi file from a set of pngs.
+    It is typically not installed by default on linux distros because of
+    legal restrictions, but it is widely available.
+    """
+
+    if limits == None:
+        LatIni = lat.min()
+        LatFin = lat.max()
+        LonIni = lon.min()
+        LonFin = lon.max()
+    else:
+        LatIni = limits['LatIni']
+        LatFin = limits['LatFin']
+        LonIni = limits['LonIni']
+        LonFin = limits['LonFin']
+
+    #try:
+    #    subprocess.check_call(['mencoder'])
+    #except subprocess.CalledProcessError:
+    #    print "mencoder command was found"
+    #    pass # mencoder is found, but returns non-zero exit as expected
+    #         # This is a quick and dirty check; it leaves some spurious output
+    #	     # for the user to puzzle over.
+    #except OSError:
+    #	 print not_found_msg
+    #     sys.exit("quitting\n")
+
+    parallels = numpy.arange(-5,20.1,5)
+    meridians = numpy.arange(300,340,10)
+    V = range(-20, 21, 1)
+    for i in range(len(t)) :
+        #fig = plt.figure(figsize=(14,10.5), dpi=100)
+        pylab.subplot(211)
+        map = Basemap(projection='merc',lat_ts=0,llcrnrlon=LonIni,llcrnrlat=LatIni, urcrnrlon=LonFin, urcrnrlat=LatFin,resolution='l',area_thresh=1000.)
+        X, Y = map(*pylab.meshgrid(lon, lat))
+        map.contourf(X,Y, data[i], V)
+        plt.colorbar(shrink=0.8)
+        map.drawcoastlines()
+        map.fillcontinents(color='0.0')
+        map.drawparallels(parallels,labels=[1,0,0,1])
+        map.drawmeridians(meridians,labels=[1,0,0,1])
+        plt.title('%s' % (t[i].strftime('%Y-%m-%d')))
+        #
+        pylab.subplot(212)
+        map = Basemap(projection='merc',lat_ts=0,llcrnrlon=LonIni,llcrnrlat=LatIni, urcrnrlon=LonFin, urcrnrlat=LatFin,resolution='l',area_thresh=1000.)
+        X, Y = map(*pylab.meshgrid(lon, lat))
+        map.contourf(X,Y, eofdata[i], V)
+        plt.colorbar(shrink=0.8)
+        map.drawcoastlines()
+        map.fillcontinents(color='0.0')
+        map.drawparallels(parallels,labels=[1,0,0,1])
+        map.drawmeridians(meridians,labels=[1,0,0,1])
+        plt.title('%s (EOF reconstructed)' % (t[i].strftime('%Y-%m-%d')))
+        # ----
+        filename = str('../tmp/%04d' % i) + '.png'
+        plt.savefig(filename, dpi=100)
+        print 'Wrote file', filename
+        #
+        # Clear the figure to make way for the next image.
+        #
+        plt.clf()
+
+    command = ('mencoder',
+               'mf://../tmp/*.png',
+               '-mf',
+               'type=png:w=800:h=600:fps=2',
+               '-ovc',
+               'lavc',
+               '-lavcopts',
+               'vcodec=mpeg4',
+               '-oac',
+               'copy',
+               '-o',
+               outputfilename)
+
+    os.spawnvp(os.P_WAIT, 'mencoder', command)
+
+    #print "\n\nabout to execute:\n%s\n\n" % ' '.join(command)
+    #subprocess.check_call(command)
+
+    print "\n\n The movie was written to 'output.avi'"
+
+    print "\n\n You may want to delete *.png now.\n\n"
+    return
