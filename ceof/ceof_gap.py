d2ee6647ddf828b0bc7490aabfe099be2b21ad13
Bia <melovillasboas@gmail.com>
2016-01-13 19:06:08 -0800

    adding function to compute the EOF

--- ceof/ceof_gap.py
+++ ceof/ceof_gap.py
@@ -1,5 +1,19 @@
 import numpy as np
+from scipy import signal
 
+def eof(data):
+    """
+    expects: data[time,positions]
+
+    """
+    data = signal.detrend(data, axis=0)
+    U, s, V = np.linalg.svd(data, full_matrices=True) # SVD analisys
+    S = np.zeros((data).shape) # We need to complete with zeros
+    N = S.shape[0]
+    S[:N, :N] = np.diag(s)
+    exp_var = s**2/np.sum(s**2) # explained variance of each mode for y1 
+    PC = np.dot(S,V) 
+    return U[:,0], PC, exp_var
 
 def cov2D_gap(x):
     """ Covariance matrix allowing gaps
