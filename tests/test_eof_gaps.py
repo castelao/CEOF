a1d7e5f30088bb3f6330280a271eb56cb69148cc
Gui <guilherme@castelao.net>
2016-01-13 20:09:51 -0800

    eof_reconstruct() to test EOF reconstruction.

--- tests/test_eof_gaps.py
+++ tests/test_eof_gaps.py
@@ -21,11 +21,20 @@ def test_eof_shapes():
     assert e.shape == (3, 3)
 
 
-def test_eof_recons():
-    x = np.random.random((4,3))
+def eof_reconstruct(x):
+    """ Decompose and reconstruct the original data
+    """
     p, v, e = eof(x)
     y = np.zeros_like(x)
     for n in v.size():
         y += (p[:, n][:,None] * e[:, n])
 
-    assert np.all_close(x, y)
+    return y
+
+
+def test_eof_recons():
+    x = np.random.random((4,3))
+    assert np.all_close(x, eof_reconstruct(x))
+
+    # FIXME: Create another test for this signal
+    # y = np.sin(2 * pi * t)[:, None] + (0.1 * np.sin(2 * pi * t/2))[:, None] * x
