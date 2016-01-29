9cbd108525e26a0e2bf34383ac817c13537c757f
Gui <guilherme@castelao.net>
2016-01-29 15:44:26 -0800

    Big mess, but it's working. Need some cleanning and organizing.

--- ceof/ceof.py
+++ ceof/ceof.py
@@ -10,6 +10,8 @@ import numpy
 import numpy as np
 from numpy import ma
 import scipy.fftpack
+import scipy.signal
+from scipy import linalg
 
 try:
     from pyclimate.svdeofs import svdeofs, getvariancefraction
@@ -18,8 +20,169 @@ except:
 
 from utils import scaleEOF
 
+"""
 
-def ceof_scalar2D(data):
+    Possible cases:
+
+       - EOF
+         - 2D
+           - array
+           - Masked Array
+         - ND (EOF 2D)
+           - array
+           - Masked Array
+       - CEOF
+         - 2D (EOF 2D)
+           - array
+           - Masked Array
+         - ND (EOF ND)
+           - array
+           - Masked Array
+
+"""
+
+"""
+
+    Core functions for EOF decomposition
+"""
+
+def eof_from_svd(data):
+    """
+    Input: 2D array
+
+    Output: PCs, explainned variance, EOFs
+
+    The output PC will be composed over the first dimension of input data,
+      while the EOF will be composed from the second dimension. For example,
+      for a series of sensors, a typical analysis is to have the time along
+      the first dimension (rows) and each sensor on the second dimension
+      (columns).
+    """
+    U, s, V = np.linalg.svd(data, full_matrices=False)  # SVD analisys
+    S = np.diag(s)
+    exp_var = s**2 / np.sum(s**2)  # explained variance of each mode for y1
+    PC = np.dot(U, S)
+
+    return PC, exp_var, V.T
+
+
+def eof_with_gaps(x):
+    """ EOF decomposition of 2D array that allows some gaps.
+
+        It's important to keep in mind that it assumes that the gaps does not
+          compromise the estimate of the covariance between the data series
+          of two positions.
+    """
+    x = np.asanyarray(x)
+    assert x.ndim == 2, "Input must be 2D"
+
+    #Cov = np.dot(x.T, x)
+    Cov = cov_with_gaps(x)
+    L, C = np.linalg.eig(Cov)
+
+    # I learnned from Eric Firing that L is not necessarily sorted
+    isort = np.argsort(L)[::-1]
+    L = L[isort]
+    C = C[:, isort]
+
+    eofs = C.T
+
+    exp_var = L/L.sum()
+
+    nmodes = L.size
+    pcs = np.empty((x.shape[0], nmodes))
+    for n in range(nmodes):
+        pcs[:, n] = np.dot(x, C[:, n])
+
+    return pcs, exp_var, eofs
+
+
+def cov_with_gaps(x):
+    """ Covariance matrix allowing gaps
+
+
+        ATENTION: Might be a good idea to set a constraint on max gaps
+          allowed, like at least 98% of the data?
+    """
+    assert x.ndim == 2, "Input must be 2D"
+
+    #assert x.any(axis=0).all(), \
+    #        "Must have at least one valid data along axis=0"
+
+    N, J = x.shape
+
+    y = np.empty((J, J), x.dtype)
+    # While I don't apply a criteria if the cov can be estimated, it will
+    #   run for all points, hence a regular np.array can do it.
+    # y = ma.masked_all((J, J), x.dtype)
+
+    for i in range(J):
+        for j in range(J):
+            # FIXME: Double check if I should use mean or N*mean
+            #   i.e. does it take the sum or the mean? I think it is the sum.
+            y[i, j] = N*(x[:, i] * x[:, j]).mean()
+
+    return y
+
+
+def eof_decompose(x):
+    """
+
+    """
+    x = np.asanyarray(x)
+
+    # If more than 2 dimensions, reshape and than re-reshape
+    if x.ndim > 2:
+        S = x.shape
+        pcs, exp_var, eofs = eof_decompose(
+                x.reshape((S[0], x.size/S[0]), order="C"))
+
+        nmodes = eofs.shape[-1]
+        eofs = eofs.reshape(list(S[1:]) + [nmodes])
+
+        return pcs, exp_var, eofs
+    # Below here it's expected only 2D input x.
+
+    mask = ~np.isfinite(np.array(x)) | ma.getmaskarray(x)
+
+    # If all data is valid, use linalg.svd is faster
+    #if (np.isfinite(x).all()) & (not ma.getmaskarray(x).any()):
+    if ~mask.any():
+        return eof_from_svd(x)
+
+    # If is there any column wihtout any valid data, remove those and
+    #   try again.
+    if mask.all(axis=0).any():
+        ind = ~mask.all(axis=0)
+        pcs, exp_var, tmp = eof_decompose(x[:,ind])
+        eofs = ma.masked_all((ind.size, pcs.shape[1]), dtype=tmp.dtype)
+        eofs[ind] = tmp
+        return pcs, exp_var, eofs
+
+    # At this point all columns have some valid data, but not all
+    return eof_with_gaps(ma.fix_invalid(x))
+
+
+def eof_reconstruct(pcs, eofs, nmodes=None):
+    """ Reconstruct the input data from pcs + eofs
+    """
+    # At this point works only with 2D arrays.
+    assert pcs.ndim == 2
+    assert eofs.ndim == 2
+    # It assumes that the modes are in last dimension.
+    assert pcs.shape(1) == eofs.shape(1)
+
+    assert (nmodes is None) or (nmodes <= pcs.shape(1))
+
+    if nmodes is None:
+        data = np.dot(pcs, eofs.T)
+    else:
+        data = np.dot(pcs[:,:nmodes], eofs[:,:nmodes].T)
+
+    return data
+
+
+def ceof_scalar2D(x):
     """ Estimate the complex EOF on a 2D array.
 
         Time should be the first dimension, so that the PC (eigenvalues) will
